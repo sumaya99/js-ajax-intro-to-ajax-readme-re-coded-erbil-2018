18856c264b0b5de89f88e9b1d9d6973812d2f338
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-14 07:47:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function showRepositories(event, data) {
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
 
 function getCommits(el) {
   const name = el.dataset.repo
